dbc37f7059d6ddac2c42dc5063fcb09458259265
Siddhartha <sid@countvajhula.com>
2025-01-02 13:21:52 -0700

    examples for languages 4 and 5

--- examples/4.rkt
+++ examples/4.rkt
@@ -1,4 +1,4 @@
-#lang raqit/3
+#lang raqit/4
 
 (require (only-in racket
                   sqr)
@@ -6,39 +6,39 @@
 
 (def a 3)
 
-(let ([a 3])
-  a)
-
-(def (b c) (values 1 2))
+(def (p q) (values 1 2))
 
 (let ([[: x xs] [1 2 3]])
+  x)
+
+(let ([b 3] [c 5])
+
+  (def [: x xs] [1 2 3])
+
   (displayln (+ a b c x)))
 
+(== #:key string-upcase "ABC" "ABC")
+
 (def w 3)
+
+(struct dog (name age))
+
+(dog "fido" 5)
+
 (cond [(< w 0) 'negative]
       [(> w 0) 'positive]
       [else 'zero])
-
-(loop go (a 5)
-  (when (> a 0)
-    (displayln a)
-    (go (sub1 a))))
-
-(loop go ([[: x xs] [1 2 3 4 5]]
-          [a 5])
-  (displayln x)
-  (unless (null? xs)
-    (go xs a)))
 
 (fun do-something (v)
   (def w (abs v))
   w)
 
-(fun do-something2 (abc [: x xs])
-  (displayln abc)
-  (displayln x))
+(fun eqwal (x y #:key k)
+  (equal? (k x) (k y)))
 
-(do-something2 "hi" [1 2 3])
+(eqwal 3 5 #:key {(remainder 3)})
+
+(~> ([1 2 3]) (esc (λ (xs) (def [: y ys] xs) y)))
 
 (flow check-number
   (switch
@@ -55,10 +55,10 @@
 (~> (3) sqr add1)
 
 (macro (where expr bindings)
-  #'(let bindings expr))
+  #'(def bindings expr))
 
 (where (+ v 3)
-       ([v 5]))
+       abc)
 
 (switch (3)
   [(< 0) 'negative]
@@ -74,15 +74,26 @@
 (class fish% (object%)
   (init size)
   (super-new)
-  (define current-size size)
-  (define/public (get-size)
+  (def current-size size)
+  (method get-size ()
     current-size)
-  (define/public (grow amt)
+  (method grow (amt)
     (set! current-size (+ amt current-size)))
-  (define/public (eat other-fish)
+  (method eat (other-fish)
     (grow (send other-fish get-size))))
 
 (define charlie
   (new fish% [size 10]))
 
 (send charlie get-size)
+
+(loop go (a 5)
+  (when (> a 0)
+    (displayln a)
+    (go (sub1 a))))
+
+(loop go ([[: x xs] [1 2 3 4 5]]
+          [a 5])
+  (displayln x)
+  (unless (null? xs)
+    (go xs a)))
--- examples/5.rkt
+++ examples/5.rkt
@@ -1,44 +1,58 @@
-#lang raqit/3
+#lang raqit/5
 
 (require (only-in racket
                   sqr)
          (for-syntax racket/base))
 
+(behavior stack
+  (push stack element)
+  (pop stack)
+  (top stack))
+
+(struct dogg (name age)
+  #:methods gen:stack
+  [(fun push (this element)
+     'hi)
+   (fun pop (this)
+     'hi)
+   (fun top (this)
+     'hi)])
+
 (def a 3)
 
-(let ([a 3])
-  a)
-
-(def (b c) (values 1 2))
+(def (p q) (values 1 2))
 
 (let ([[: x xs] [1 2 3]])
+  x)
+
+(let ([b 3] [c 5])
+
+  (def [: x xs] [1 2 3])
+
   (displayln (+ a b c x)))
 
+(== #:key string-upcase "abc" "ABC")
+
 (def w 3)
+
+(struct dog (name age))
+
+(dog "fido" 5)
+
 (cond [(< w 0) 'negative]
       [(> w 0) 'positive]
       [else 'zero])
-
-(loop go (a 5)
-  (when (> a 0)
-    (displayln a)
-    (go (sub1 a))))
-
-(loop go ([[: x xs] [1 2 3 4 5]]
-          [a 5])
-  (displayln x)
-  (unless (null? xs)
-    (go xs a)))
 
 (fun do-something (v)
   (def w (abs v))
   w)
 
-(fun do-something2 (abc [: x xs])
-  (displayln abc)
-  (displayln x))
+(fun eqwal (x y #:key k)
+  (equal? (k x) (k y)))
 
-(do-something2 "hi" [1 2 3])
+(eqwal 3 5 #:key {(remainder 3)})
+
+(~> ([1 2 3]) (esc (λ (xs) (def [: y ys] xs) y)))
 
 (flow check-number
   (switch
@@ -55,10 +69,10 @@
 (~> (3) sqr add1)
 
 (macro (where expr bindings)
-  #'(let bindings expr))
+  #'(def bindings expr))
 
 (where (+ v 3)
-       ([v 5]))
+       abc)
 
 (switch (3)
   [(< 0) 'negative]
@@ -71,18 +85,38 @@
 
 (hello "sid")
 
+(macro (rev f g)
+  #'{~> g f})
+
+(map (rev add1 sqr) [1 2 3])
+
 (class fish% (object%)
   (init size)
   (super-new)
-  (define current-size size)
-  (define/public (get-size)
+  (def current-size size)
+  (method get-size ()
     current-size)
-  (define/public (grow amt)
+  (method grow (amt)
     (set! current-size (+ amt current-size)))
-  (define/public (eat other-fish)
+  (method eat (other-fish)
     (grow (send other-fish get-size))))
 
 (define charlie
   (new fish% [size 10]))
 
 (send charlie get-size)
+
+(loop go (a 5)
+  (when (> a 0)
+    (displayln a)
+    (go (sub1 a))))
+
+(loop go ([[: x xs] [1 2 3 4 5]]
+          [a 5])
+  (displayln x)
+  (unless (null? xs)
+    (go xs a)))
+
+(~ [1 2 3] [4 5 6] [7 8 9])
+
+(~ "abc" "def" "ghijk")
