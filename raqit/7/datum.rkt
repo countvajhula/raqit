0a5842286d11cbc9272175bed1e45bc11f45bfd4
Siddhartha <sid@countvajhula.com>
2025-02-26 21:52:36 -0700

    have literals be unquoted, I think

---
+++ raqit/7/datum.rkt
@@ -0,0 +1,22 @@
+#lang racket/base
+
+(provide #%datum)
+
+(require (only-in racket/base
+                  [#%datum racket:datum])
+         syntax/parse/define
+         (for-syntax racket/base)
+         (for-syntax "strip-context.rkt")
+         racket/set)
+
+(define-syntax-parser #%datum
+  [(#%datum . hsh)
+   #:when (syntax-property #'hsh 'raqit-hash-map)
+   #:with (e ...) (replace-context #'hsh (syntax-property #'hsh 'raqit-hash-map))
+   (syntax/loc this-syntax (hash e ...))]
+  [(#%datum . st)
+   #:when (syntax-property #'st 'raqit-set)
+   #:with (e:expr ...) (replace-context #'st (syntax-property #'st 'raqit-set))
+   (syntax/loc this-syntax (set e ...))]
+  [(#%datum . e)
+   (syntax/loc this-syntax (racket:datum . e))])
