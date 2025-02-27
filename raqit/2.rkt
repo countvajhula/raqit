c713154cc35a4ca0810cd6b8ed07ee562422bc9b
Siddhartha <sid@countvajhula.com>
2025-02-26 21:46:11 -0700

    move lang 6 to toplevel

--- raqit/2.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-
-(require "2/expander.rkt")
-(provide (all-from-out "2/expander.rkt"))
-
-(module reader racket/base
-  (require "2/reader.rkt")
-  (provide (all-from-out "2/reader.rkt")))
--- raqit/4.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-
-(require "4/expander.rkt")
-(provide (all-from-out "4/expander.rkt"))
-
-(module reader racket/base
-  (require "4/reader.rkt")
-  (provide (all-from-out "4/reader.rkt")))
