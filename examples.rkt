c0c9a78bbbc99a1cb4d67079ec04982eeace308a
Siddhartha <sid@countvajhula.com>
2025-02-26 21:39:40 -0700

    move lang 3 to toplevel

--- examples.rkt
+++ examples.rkt
@@ -1,58 +1,44 @@
-#lang raqit/5
+#lang raqit
 
 (require (only-in racket
                   sqr)
          (for-syntax racket/base))
 
-(behavior stack
-  (push stack element)
-  (pop stack)
-  (top stack))
-
-(struct dogg (name age)
-  #:methods gen:stack
-  [(fun push (this element)
-     'hi)
-   (fun pop (this)
-     'hi)
-   (fun top (this)
-     'hi)])
-
 (def a 3)
 
-(def (p q) (values 1 2))
+(let ([a 3])
+  a)
+
+(def (b c) (values 1 2))
 
 (let ([[: x xs] [1 2 3]])
-  x)
-
-(let ([b 3] [c 5])
-
-  (def [: x xs] [1 2 3])
-
   (displayln (+ a b c x)))
 
-(== #:key string-upcase "abc" "ABC")
-
 (def w 3)
-
-(struct dog (name age))
-
-(dog "fido" 5)
-
 (cond [(< w 0) 'negative]
       [(> w 0) 'positive]
       [else 'zero])
+
+(loop go (a 5)
+  (when (> a 0)
+    (displayln a)
+    (go (sub1 a))))
+
+(loop go ([[: x xs] [1 2 3 4 5]]
+          [a 5])
+  (displayln x)
+  (unless (null? xs)
+    (go xs a)))
 
 (fun do-something (v)
   (def w (abs v))
   w)
 
-(fun eqwal (x y #:key k)
-  (equal? (k x) (k y)))
+(fun do-something2 (abc [: x xs])
+  (displayln abc)
+  (displayln x))
 
-(eqwal 3 5 #:key {(remainder 3)})
-
-(~> ([1 2 3]) (esc (λ (xs) (def [: y ys] xs) y)))
+(do-something2 "hi" [1 2 3])
 
 (flow check-number
   (switch
@@ -69,10 +55,10 @@
 (~> (3) sqr add1)
 
 (macro (where expr bindings)
-  #'(def bindings expr))
+  #'(let bindings expr))
 
 (where (+ v 3)
-       abc)
+       ([v 5]))
 
 (switch (3)
   [(< 0) 'negative]
@@ -85,38 +71,18 @@
 
 (hello "sid")
 
-(macro (rev f g)
-  #'{~> g f})
-
-(map (rev add1 sqr) [1 2 3])
-
 (class fish% (object%)
   (init size)
   (super-new)
-  (def current-size size)
-  (method get-size ()
+  (define current-size size)
+  (define/public (get-size)
     current-size)
-  (method grow (amt)
+  (define/public (grow amt)
     (set! current-size (+ amt current-size)))
-  (method eat (other-fish)
+  (define/public (eat other-fish)
     (grow (send other-fish get-size))))
 
 (define charlie
   (new fish% [size 10]))
 
 (send charlie get-size)
-
-(loop go (a 5)
-  (when (> a 0)
-    (displayln a)
-    (go (sub1 a))))
-
-(loop go ([[: x xs] [1 2 3 4 5]]
-          [a 5])
-  (displayln x)
-  (unless (null? xs)
-    (go xs a)))
-
-(~ [1 2 3] [4 5 6] [7 8 9])
-
-(~ "abc" "def" "ghijk")
--- raqit/6.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket/base
-
-(require "6/expander.rkt")
-(provide (all-from-out "6/expander.rkt"))
-
-(module reader racket/base
-  (require "6/reader.rkt")
-  (provide (all-from-out "6/reader.rkt")))
