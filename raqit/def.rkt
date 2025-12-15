e8be7f65c337287e43edf3e51df6bf89c613f2cc
Siddhartha <sid@countvajhula.com>
2025-12-15 14:00:12 -0700

    use `:` in both expression and pattern matching positions
    Looks like this can be done using define-match-expander (formerly they
    were colliding as identifiers when separately defined, necessitating
    `:` for pattern matching and `::` for expressions).

--- raqit/def.rkt
+++ raqit/def.rkt
@@ -2,17 +2,10 @@
 
 (provide def)
 
-(provide def
-         :)
-
 (require racket/match
          syntax/parse/define
-         syntax/parse
          (for-syntax racket/base
                      syntax/parse/class/paren-shape))
-
-(define-match-expander :
-  (syntax-parser [(: v ... vs) #'(list* v ... vs)]))
 
 (define-syntax-parser def
   [(_ (~parens pat ...) values-expr)
--- raqit/list.rkt
+++ raqit/list.rkt
@@ -1,14 +1,21 @@
 #lang racket/base
 
 (provide (rename-out [squarelist-app #%app])
-         ::)
+         :)
 
-(require (for-syntax racket/base
+(require racket/match
+         (for-syntax racket/base
                      syntax/parse
                      syntax/parse/class/paren-shape)
          (only-in racket [#%app racket:app]))
 
-(define :: list*)
+(define-match-expander :
+  (syntax-parser
+    [(: v ... vs)
+     #'(list* v ... vs)])
+  (syntax-parser
+    [(_ v ...)
+     #'(list* v ...)]))
 
 (define-syntax squarelist-app
   (syntax-parser
