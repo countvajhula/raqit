e6af677461ca4ca86ab9e2ddf657a6efb5680c63
Siddhartha <sid@countvajhula.com>
2025-12-16 14:04:13 -0700

    use subset matching in set and hash patterns
    We don't need the pattern matching forms to assert the stronger
    relation of equality, and subset matching is perhaps more distinctly
    useful, while being intuitive.

--- raqit/hash.rkt
+++ raqit/hash.rkt
@@ -10,7 +10,8 @@
   ;; use in pattern-matching context
   (syntax-parser
     [(_ v ...)
-     #'(hash v ...)])
+     ;; Use #:open to allow extra keys (subset matching)
+     #'(hash v ... #:open)])
 
   ;; use in expression context
   (syntax-parser
--- raqit/set.rkt
+++ raqit/set.rkt
@@ -9,10 +9,11 @@
          (for-syntax racket/base))
 
 (define-match-expander #%set
-  ;; use in pattern-matching context
+  ;; use in pattern-matching context (subset matching)
   (syntax-parser
     [(_ v ...)
-     #'(? set? (app set->list (list-no-order v ...)))])
+     ;; The `_ ...` allows extra elements to exist in the set
+     #'(? set? (app set->list (list-no-order v ... _ (... ...))))])
   ;; use in expression context
   (syntax-parser
     [(_ v ...)
