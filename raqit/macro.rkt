746da32c5f87909ef29cc6c75eab748b1d359a4f
Siddhartha <sid@countvajhula.com>
2025-12-17 12:41:09 -0700

    unify macro definitions with `macro`
    We now define both host-language as well as Qi macros using the same
    `macro` form, by recognizing reader-level syntax for these macros. In
    the future, the same `macro` form could likewise be extended to
    support defining macros for other hosted DSLs.

--- raqit/macro.rkt
+++ raqit/macro.rkt
@@ -3,14 +3,41 @@
 (provide macro)
 
 (require syntax/parse/define
-         (for-syntax racket/base))
+         (for-syntax racket/base)
+         qi)
 
 (define-syntax-parser macro
-  [(_ (name pattern ...) template)
+  ;; ---------------------------------------------------------
+  ;; CASE 1: Qi Macros (Tagged with ☯)
+  ;; Reader produces: (macro (#%flow HEAD) . BODY)
+  ;; ---------------------------------------------------------
+
+  ;; 1A. Single Clause Shorthand: ☯(name args ...)
+  ;; The template is implicitly quoted, like Racket's
+  ;; single-clause macro-defining forms
+  [(_ ((~datum #%flow) (name:id args ...)) body ...)
+   #'(define-qi-syntax-rule (name args ...)
+       body ...)]
+
+  ;; 1B. Full Form: ☯name
+  ;; The templates require explicit #'.
+  [(_ ((~datum #%flow) name:id) body ...)
+   #'(define-qi-syntax-parser name
+       body ...)]
+
+  ;; ---------------------------------------------------------
+  ;; CASE 2: Host-language Macros (Untagged)
+  ;; Reader produces: (macro HEAD . BODY)
+  ;; ---------------------------------------------------------
+
+  ;; 2A. Single Clause Shorthand: (name args ...)
+  ;; The template is implicitly quoted
+  [(_ (name:id args ...) body ...)
+   #'(define-syntax-parse-rule (name args ...)
+       body ...)]
+
+  ;; 2B. Full Form: name
+  ;; The templates require explicit #'.
+  [(_ name:id body ...)
    #'(define-syntax-parser name
-       [(_ pattern ...) template])]
-  [(_ name:id parse-option ... [pattern template] ...)
-   #'(define-syntax-parser name
-       parse-option ...
-       [pattern template]
-       ...)])
+       body ...)])
--- raqit/qi.rkt
+++ raqit/qi.rkt
@@ -3,20 +3,9 @@
 (provide (except-out (all-from-out qi)
                      flow)
          (rename-out [flow #%flow])
-         (all-from-out qi/list)
-         flow-macro)
+         (all-from-out qi/list))
 
 (require qi/list
          qi
          syntax/parse/define
          (for-syntax racket/base))
-
-(define-syntax-parser flow-macro
-  [(_ (name pattern ...) template)
-   #'(define-qi-syntax-parser name
-       [(_ pattern ...) template])]
-  [(_ name:id parse-option ... [pattern template] ...)
-   #'(define-qi-syntax-parser name
-       parse-option ...
-       [pattern template]
-       ...)])
