58b2af559b7d83c2d4917e04ac094b5e817c12f8
Siddhartha <sid@countvajhula.com>
2025-12-17 17:40:15 -0700

    support `(implements protocol method ...)` syntax in structs
    This grouping is more tractable than Racket's built-in `#:methods`
    syntax which implicitly consumes the following two arguments (atypical
    for a keyword argument) without any explicit grouping. It also makes
    it easy for us to use the known context of the `(implements ...)` form
    to expect just the name of the protocol (nee generic interface) rather
    than the exact binding, e.g., just `(implements stack ...)` rather
    than `(implements gen:stack ...)`.

--- raqit/struct.rkt
+++ raqit/struct.rkt
@@ -4,14 +4,42 @@
 
 (require syntax/parse/define
          (prefix-in b: racket/base)
-         (for-syntax racket/base))
+         (for-syntax racket/base
+                     racket/syntax))
 
-;; (define-syntax-parameter implements
-;;   (lambda (stx)
-;;     (raise-syntax-error (syntax-e stx) "can only be used inside `struct`")))
+(begin-for-syntax
+  (define-syntax-class struct-option
+    ;; We specify depth 1 because 'expanded' will always hold a LIST of syntax objects.
+    ;; This allows us to use '... ...' (double ellipsis) in the main macro to splice them flat.
+    #:attributes ([expanded 1])
+
+    ;; Case 1: The 'implements' clause
+    ;; Syntax: (implements protocol-id method ...)
+    ;; Rewrites to: (#:methods gen:protocol-id [method ...])
+    (pattern ((~datum implements) interface:id method ...)
+             ;; 1. Construct the "gen:interface" identifier.
+             ;; We use #'interface as the 'context', ensuring we look for the binding
+             ;; in the same scope where the user wrote the name.
+             #:with gen-id (format-id #'interface "gen:~a" #'interface)
+
+             ;; 2. Expand using the computed gen-id
+             #:with (expanded ...) #'(#:methods gen-id [method ...]))
+
+    ;; Case 2: Standard struct options (pass-through)
+    ;; Syntax: #:mutable, #:property, #:guard, etc.
+    ;; Rewrites to: (the-option-itself)
+    ;; We wrap 'other' in a list so it matches the list-shape of Case 1.
+    (pattern other
+             #:with (expanded ...) #'(other))))
 
 (define-syntax-parser struct
-  [(_ id (field ...) struct-option ...)
-   #'(b:struct id (field ...) #:transparent struct-option ...)]
-  [(_ id super (field ...) struct-option ...)
-   #'(b:struct id super (field ...) #:transparent struct-option ...)])
+  ;; 1. Struct without superclass
+  ;; Matches: (struct point (x y) #:mutable (implements gen:custom ...))
+  [(_ id:id (field ...) opt:struct-option ...)
+   ;; Use 'opt.expanded ... ...' to flatten the list-of-lists into a single sequence of options
+   #'(b:struct id (field ...) #:transparent opt.expanded ... ...)]
+
+  ;; 2. Struct with superclass
+  ;; Matches: (struct 3d-point point (z) ...)
+  [(_ id:id super:id (field ...) opt:struct-option ...)
+   #'(b:struct id super (field ...) #:transparent opt.expanded ... ...)])
