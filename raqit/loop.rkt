5f87faa86320d6e706480f3cd65a0d4b55a83f95
Siddhartha <sid@countvajhula.com>
2025-12-19 21:30:30 -0700

    improve `loop` error reporting

--- raqit/loop.rkt
+++ raqit/loop.rkt
@@ -6,6 +6,15 @@
          syntax/parse/define
          (for-syntax racket/base))
 
+(begin-for-syntax
+  (define-syntax-class binding-pair
+    #:description "binding pair (e.g., [x 5])"
+    (pattern (pat val)))
+
+  (define-syntax-class binding-list
+    #:description "list of binding pairs (e.g., ([x 1] [y 2]))"
+    (pattern (pair:binding-pair ...))))
+
 (define-syntax-parser loop
-  [(_ name:id arg ...)
-   #'(match-let name arg ...)])
+  [(_ name:id bindings:binding-list body ...+)
+   #'(match-let name bindings body ...)])
