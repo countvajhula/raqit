f9eb620934786a9cc2f79094540f016663410987
Siddhartha <sid@countvajhula.com>
2026-04-01 23:12:24 -0700

    handle literal data such as lists correctly in flow reader syntax

--- raqit/qi.rkt
+++ raqit/qi.rkt
@@ -1,11 +1,24 @@
 #lang racket/base
 
-(provide (except-out (all-from-out qi)
-                     flow)
-         (rename-out [flow #%flow])
+(provide (except-out (all-from-out qi) flow)
+         (rename-out [raqit-flow #%flow])
          (all-from-out qi/list))
 
 (require qi/list
          qi
          syntax/parse/define
          (for-syntax racket/base))
+
+(define-syntax-parser raqit-flow
+  [(_ expr)
+   (define shape (syntax-property #'expr 'paren-shape))
+
+   ;; If the paren shape is explicitly square brackets
+   ;; or curly braces, wrap the expression in Qi's 'gen'
+   ;; form before passing it on, so that it gets
+   ;; handled by Raqit's #%app macro (e.g., [...] is a list)
+   (if (memv shape '(#\[ #\{))
+       #'(flow (gen expr))
+       ;; Otherwise (round parens or no shape, like an identifier),
+       ;; pass it through to Qi untouched.
+       #'(flow expr))])
