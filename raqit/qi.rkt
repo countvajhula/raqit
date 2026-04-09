cb84cd8b8c3fb8bbe13f0d0dd672360c8095581d
Siddhartha <sid@countvajhula.com>
2026-04-09 00:55:52 -0700

    fix logic literals and lambda escape in Qi floe positions

--- raqit/qi.rkt
+++ raqit/qi.rkt
@@ -2,14 +2,39 @@
 
 (provide (except-out (all-from-out qi) flow)
          (rename-out [raqit-flow #%flow])
-         (all-from-out qi/list))
+         (all-from-out qi/list)
+         (for-space qi
+                    lambda
+                    λ
+                    void
+                    the-any
+                    the-all))
 
 (require qi/list
          qi
          (prefix-in b: racket/base)
-         "void.rkt"
+         (prefix-in l: "void.rkt")
+         (prefix-in l: "logic.rkt")
+         (prefix-in l: "lambda.rkt")
          syntax/parse/define
-         (for-syntax racket/base))
+         (for-syntax racket/base
+                     syntax/parse))
+
+
+(define-qi-syntax-rule (lambda e ...)
+  (esc (lambda e ...)))
+
+(define-qi-syntax-rule (λ e ...)
+  (lambda e ...))
+
+(define-qi-syntax-parser void
+  [_:id #'(gen l:void)])
+
+(define-qi-syntax-parser the-any
+  [_:id #'(gen l:the-any)])
+
+(define-qi-syntax-parser the-all
+  [_:id #'(gen l:the-all)])
 
 (define-syntax-parser raqit-flow
   ;; Intercept the void literal, i.e., ☯void
